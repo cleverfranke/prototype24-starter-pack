e9276cd41c494b73c29c42f0c5533bb3241ffe13
Betty <betty.k1995@gmail.com>
2024-09-19 09:25:19 +0200

    update screen dimensions

--- src/helpers.js
+++ src/helpers.js
@@ -1,7 +1,7 @@
 // Function to calculate and set the responsive canvas dimensions and scaling factor
 let sketchHeight, sketchWidth; // Variables to store the calculated canvas dimensions
 
-let ratio = 3112 / 4096; // Aspect ratio of the sketch (width/height)
+let ratio = 2160 / 3840; // Aspect ratio of the sketch (width/height)
 let baseWidth = 1000;
 let base = 0;
 
